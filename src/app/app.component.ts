15febdc8bcde6b7e86cf8555744bb6681e7865c7
Mohamed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-12-22 18:07:58 +0200

    Rendering a List of Articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
 
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homapage', 'http://angular.io', 1),
+    ];
+  }
+  
   addArticle(title: HTMLInputElement, link: HTMLInputElement): Boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
